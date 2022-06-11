04bf171343203b37378ce839d0069c498ef4517f
amogussus6969 <samdesl79@gmail.com>
2022-06-11 12:58:45 -0400

    Condition System

---
+++ main.py
@@ -0,0 +1,50 @@
+#Cube a programming language
+
+
+
+
+
+
+def condition(arg1, comparasion, arg2, code):
+    if comparasion == "==":
+        if arg1 == arg2:
+            compile(code)
+            return True
+        else:
+            return False
+    if comparasion == "<<":
+        if arg1 << arg2:
+            compile(code)
+            return True
+        else:
+            return False
+    if comparasion == ">>":
+        if arg1 >> arg2:
+            compile(code)
+            return True
+        else:
+            return False
+    if comparasion == ">=":
+        if arg1 >= arg2:
+            compile(code)
+            return True
+        else:
+            return False
+    if comparasion == "<=":
+        if arg1 <= arg2:
+            compile(code)
+            return True
+        else:
+            return False
+    if comparasion == "not==":
+        if not arg1 == arg2:
+            compile(code)
+            return True
+        else:
+            return False
+    if comparasion == "not>>":
+        if not arg1 >> arg2:
+            compile(code)
+            return True
+        else:
+            return False
