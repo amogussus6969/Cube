0eb8b2bc8b70452bde0014a51286bb7691d02035
amogussus6969 <samdesl79@gmail.com>
2022-06-11 13:03:24 -0400

    Finished All Comparasions

--- main.py
+++ main.py
@@ -54,3 +54,15 @@ def condition(arg1, comparasion, arg2, code):
             return True
         else:
             return False
+    if comparasion == "not<=":
+        if not arg1 <= arg2:
+            compile(code)
+            return True
+        else:
+            return False
+    if comparasion == "not>=":
+        if not arg1 >= arg2:
+            compile(code)
+            return True
+        else:
+            return False
